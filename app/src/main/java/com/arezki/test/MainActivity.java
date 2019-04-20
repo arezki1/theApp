7ea19d8338099cb56956c84f5dbe92a99ecb5412
arezki1 <a.ibsaine@hotmail.fr>
2019-04-20 21:07:55 +0100

    Initial commit

---
+++ app/src/main/java/com/arezki/test/MainActivity.java
@@ -0,0 +1,17 @@
+package com.arezki.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
